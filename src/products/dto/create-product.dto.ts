69860c2a50f971c406f57b74a24f6d7cc8d4b48d
Bluebberies <francisalexander000@gmail.com>
2024-11-26 22:10:12 +0100

    updated prodct creation dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsOptional, IsNotEmpty, ValidateIf, ValidateNested } from 'class-validator';
+import { IsString, IsOptional, IsNotEmpty, ValidateIf } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 export class CreateProductDto {
   @IsString()
@@ -37,26 +37,29 @@ export class CreateProductDto {
   @ApiProperty({ description: 'Product category Id of the product' })
   categoryId: string;
 
-  // @ValidateIf((obj) => typeof obj.inventoryBatchId === 'string')
-  // // @IsString()
-  // @IsNotEmpty()
+  @ValidateIf((obj) => typeof obj.inventoryBatchId === 'string')
+  // @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description:
+      'The Inventory IDs associated with the product. Can be a single comma separated string of IDs or an array of inventoryBatchId strings.',
+    oneOf: [{ type: 'string' }, { type: 'array', items: { type: 'string' } }],
+    example: '12345,67890', // Example of string format
+  })
+  inventoryBatchId: string | string[];
+
   // @ApiProperty({
   //   description:
   //     'The Inventory IDs associated with the product. Can be a single string or an array of inventoryBatchId strings.',
   //   oneOf: [{ type: 'string' }, { type: 'array', items: { type: 'string' } }],
-  //   example: '12345,67890', // Example of string format
+  //   example: ['12345', '67890'], // Example of array format
   // })
+  // @Transform(({ value }) => (typeof value === 'string' ? [value] : value), {
+  //   toClassOnly: true,
+  // })
+  // @IsNotEmpty()
+  // @IsString({ each: true }) // Ensures each item in the array is a string
   // inventoryBatchId: string | string[];
-
-  @ApiProperty({
-    description:
-      'The Inventory IDs associated with the product. Can be a single string or an array of inventoryBatchId strings.',
-    oneOf: [{ type: 'string' }, { type: 'array', items: { type: 'string' } }],
-    example: ['12345', '67890'], // Example of array format
-  })
-  @IsNotEmpty()
-  @IsString({ each: true }) // Ensures each item in the array is a string
-  inventoryBatchId: string | string[];
 
   @ApiProperty({ type: 'file' })
   productImage: Express.Multer.File;
