eb2a07b3c86efefb0d9840f01c93c67964e4ab9a
Bluebberies <francisalexander000@gmail.com>
2025-01-30 16:13:03 +0100

    updated services and models

--- src/device/device.service.ts
+++ src/device/device.service.ts
@@ -6,6 +6,7 @@ import { createReadStream } from 'fs';
 import * as csvParser from 'csv-parser';
 import { MESSAGES } from '../constants';
 import { Prisma } from '@prisma/client';
+import { ListDevicesQueryDto } from './dto/list-devices.dto';
 
 @Injectable()
 export class DeviceService {
@@ -34,8 +35,92 @@ export class DeviceService {
     });
   }
 
-  async fetchDevices() {
-    return await this.prisma.device.findMany();
+  async devicesFilter(
+    query: ListDevicesQueryDto,
+  ): Promise<Prisma.DeviceWhereInput> {
+    const {
+      search,
+      serialNumber,
+      startingCode,
+      key,
+      hardwareModel,
+      isTokenable,
+      createdAt,
+      updatedAt,
+    } = query;
+
+    const filterConditions: Prisma.DeviceWhereInput = {
+      AND: [
+        search
+          ? {
+              OR: [
+                { serialNumber: { contains: search, mode: 'insensitive' } },
+                { startingCode: { contains: search, mode: 'insensitive' } },
+                { key: { contains: search, mode: 'insensitive' } },
+                { hardwareModel: { contains: search, mode: 'insensitive' } },
+              ],
+            }
+          : {},
+        serialNumber
+          ? { serialNumber: { contains: serialNumber, mode: 'insensitive' } }
+          : {},
+        startingCode
+          ? { startingCode: { contains: startingCode, mode: 'insensitive' } }
+          : {},
+        key ? { key: { contains: key, mode: 'insensitive' } } : {},
+        hardwareModel
+          ? { hardwareModel: { contains: hardwareModel, mode: 'insensitive' } }
+          : {},
+        isTokenable
+          ? {
+              isTokenable,
+            }
+          : {},
+        createdAt ? { createdAt: new Date(createdAt) } : {},
+        updatedAt ? { updatedAt: new Date(updatedAt) } : {},
+      ],
+    };
+
+    return filterConditions;
+  }
+
+  async fetchDevices(query: ListDevicesQueryDto) {
+    const { page = 1, limit = 100, sortField, sortOrder } = query;
+
+    const filterConditions = await this.devicesFilter(query);
+
+    const pageNumber = parseInt(String(page), 10);
+    const limitNumber = parseInt(String(limit), 10);
+
+    const skip = (pageNumber - 1) * limitNumber;
+    const take = limitNumber;
+
+    const orderBy = sortField
+      ? {
+          [sortField]: sortOrder || 'asc',
+        }
+      : undefined;
+
+    const totalCount = await this.prisma.device.count({
+      where: filterConditions,
+    });
+
+    const result = await this.prisma.device.findMany({
+      skip,
+      take,
+      where: {
+        ...filterConditions,
+      },
+      orderBy,
+    });
+
+    return {
+      devices: result,
+      total: totalCount,
+      page,
+      limit,
+      totalPages: limitNumber === 0 ? 0 : Math.ceil(totalCount / limitNumber),
+    };
   }
 
   async fetchDevice(fieldAndValue: Prisma.DeviceWhereUniqueInput) {
--- src/device/dto/create-device.dto.ts
+++ src/device/dto/create-device.dto.ts
@@ -1,22 +1,28 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsString, IsOptional, IsBoolean } from 'class-validator';
+import { IsString, IsOptional, IsBoolean, MinLength } from 'class-validator';
 
 export class CreateDeviceDto {
   @ApiProperty({ description: 'Serial number of the device' })
   @IsString()
+  @MinLength(2)
   serialNumber: string;
 
   @ApiProperty({ description: 'Key associated with the device' })
   @IsString()
+  @MinLength(2)
   key: string;
 
   @ApiProperty({ description: 'Optional starting code', required: false })
   @IsString()
+  @MinLength(2)
   @IsOptional()
   startingCode?: string;
 
   @ApiProperty({ description: 'Optional count', required: false })
   @IsString()
+  @MinLength(2)
+  @MinLength(2)
+  @MinLength(2)
   @IsOptional()
   count?: string;
 
@@ -32,11 +38,13 @@ export class CreateDeviceDto {
 
   @ApiProperty({ description: 'Optional hardware model', required: false })
   @IsString()
+  @MinLength(2)
   @IsOptional()
   hardwareModel?: string;
 
   @ApiProperty({ description: 'Optional firmware version', required: false })
   @IsString()
+  @MinLength(2)
   @IsOptional()
   firmwareVersion?: string;
 
