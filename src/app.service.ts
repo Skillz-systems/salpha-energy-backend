802d6020d3e86b8413bfc628f7ddc440e2d08fea
Bluebberies <francisalexander000@gmail.com>
2025-06-20 16:34:44 +0100

    fc

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,7 @@ export class AppService {
    private readonly logger = new Logger(AppService.name);
   getHello(): string {
 
-    return 'Welcom to A4t!proosalpha';
+    return 'Welcom to Aproosalpha';
   }
 
   //   @Cron(CronExpression.EVERY_30_SECONDS)
